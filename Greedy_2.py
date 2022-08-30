eed5fd032fe448fb95f1c62835af730458970ea7
anydevil0812 <anydevil0812@naver.com>
2022-08-30 14:00:56 +0900

    그리디 알고리즘(큰 수의 법칙)

--- Greedy_2.py
+++ Greedy_2.py
@@ -1,1 +1,15 @@
-hello world
+# a = 리스트 원소의 갯수, b = 몇개의 숫자를 더할건지, c = 같은 숫자가 연속으로 몇번 나올수 있는지
+# 합이 가장 크게 나올 수 있도록 코드 짜기
+a, b, c = map(int, input().split(' '))
+li = list(map(int, input().split(' ')))
+li.sort()
+first = li[a-1]
+second = li[a-2]
+count = int(b/(c+1)) * c
+count += b % (c+1)
+
+result = 0
+result += count * first
+result += (b - count) * second
+
+print(result)
