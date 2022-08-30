6f219ba3c7412822be6396654a4fde91d9db8703
anydevil0812 <anydevil0812@naver.com>
2022-08-30 18:49:00 +0900

    그리디 알고리즘 2 (큰 수의 법칙)

--- Greedy_2.py
+++ Greedy_2.py
@@ -3,10 +3,10 @@
 a, b, c = map(int, input().split(' '))
 li = list(map(int, input().split(' ')))
 li.sort()
-first = li[a-1]
-second = li[a-2]
-count = int(b/(c+1)) * c
-count += b % (c+1)
+first = li[a-1] # 가장 큰 수
+second = li[a-2] # 두번째로 큰 수
+count = int(b/(c+1)) * c # 수열(first b번+second 1번)이 반복되는 횟수
+count += b % (c+1) # 가장 큰수가 나올 수 있는 최대 횟수
 
 result = 0
 result += count * first
