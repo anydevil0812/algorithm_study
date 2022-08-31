63925566f558cf246ffd4875fc2a37f2d4df4270
anydevil0812 <anydevil0812@naver.com>
2022-08-31 21:18:06 +0900

    시각

--- Implementation/Implementation_2.py
+++ Implementation/Implementation_2.py
@@ -1,1 +1,9 @@
-dasadsdsa
+# 0시 0분 0초부터 a시 59분 59초까지의 시각 중에서 3이 하나라도 포함되는 경우의 수
+a = int(input())
+count = 0
+for i in range(a+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+print(count)
