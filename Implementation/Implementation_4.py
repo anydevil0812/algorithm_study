a1a8e74354b9b43e766ceabbf28de7c2cb2562f8
anydevil0812 <anydevil0812@naver.com>
2022-09-01 18:53:39 +0900

    게임 개발

--- Implementation/Implementation_4.py
+++ Implementation/Implementation_4.py
@@ -1,4 +1,23 @@
 a, b = map(int, input().split())
-x, y, direction = map(int, input().split())
+ground = [[0] * b for i in range(a)] # aXb 맵 생성
+x, y, d = map(int, input().split())
+ground[x][y] = 1
+dx = [-1, 0, 1, 0]
+dy = []
+li =[]
+def v():
+    
 for i in range(a):
-    list(input().split())
+    m = list(map(int, input().split()))
+    li.append(m)
+x += 1
+y += 1
+for
+    if d == 0:
+        y += 1
+    elif d == 1:
+        x += 1
+    elif d == 2:
+        y -= 1
+    elif d == 3:
+        x -= 1
