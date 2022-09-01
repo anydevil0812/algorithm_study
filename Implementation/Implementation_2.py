74f03e5f7ba08a09f73be0ab2c95d3561f0a94b0
anydevil0812 <anydevil0812@naver.com>
2022-09-01 13:56:43 +0900

    왕실의 나이트

--- Implementation/Implementation_2.py
+++ Implementation/Implementation_2.py
@@ -1,1 +1,14 @@
-dasadsdsa
+dic = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8}
+location = input()
+count = 0
+method = [(-2,-1),(-2,1),(-1,-2),(-1,2),(1,-2),(1,2),(2,-1),(2,1)]
+
+for i in range(len(method)):
+    x = dic.get(location[0])
+    y = int(location[1])
+    x += method[i][0]
+    y += method[i][1]
+    if (0 < x < 9) and (0 < y < 9):
+        count += 1
+
+print(count)
