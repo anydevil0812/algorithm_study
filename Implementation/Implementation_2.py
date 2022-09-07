ff656bc8a72395d94a90cf9c9b0924f452f1fc6d
anydevil0812 <anydevil0812@naver.com>
2022-09-07 13:28:57 +0900

    시각

---
+++ Implementation/Implementation_2.py
@@ -0,0 +1,8 @@
+a = int(input())
+count = 0
+for i in range(a+1):
+    for j in range(60):
+        for k in range(60):
+            if ('3' in str(i)) or ('3' in str(j)) or ('3' in str(k)):
+                count += 1
+print(count)
